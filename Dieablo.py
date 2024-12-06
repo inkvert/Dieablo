89b9049e456a0da4a3f117ca1e84232a350312c2
Jon V <168088760+inkvert@users.noreply.github.com>
2024-12-06 02:08:16 +0000

    Update Dieablo.py
    Added cumulative weighting and adjusted item drop tables.

--- Dieablo.py
+++ Dieablo.py
@@ -4,12 +4,10 @@ Dieablo - a die rolling action role playing game
 Obtain lucky loot so that you can get luckier and obtain luckier loot. Thinking outside the Skinner Box.
 
 Notes:
-1) Rewrote and cleaned code to incorporate helper functions. Added proof for rolls on item generation for testing.
+1) Added cumulative weighting and adjusted item drop tables.
 2) To do: add functionality to "save"/"equip" items under equipped_items
 3) To do: once items are saved/equipped, add this value to rolls as a sort of "player power level"
-4) To do: add "weighting" to item rolls (may involve considerable rewrite to item rolling code)
-5) To do: make player power level affect item roll weighting
-6) To do: add functionality to save games
+4) To do: add functionality to save games
 
 See sample equipped item code below:
     # Dictionary which records equipped items
@@ -21,98 +19,93 @@ See sample equipped item code below:
     "jewellery_name": "0",
     "jewellery_power": 0
     }
-
- Extra note to self: Draft weighting code saved in separate file, to incorporate.
-
 """
 
 import random
 
 # Program version and tagline
-VERSION = "Pre-alpha 0.3"
+VERSION = "Pre-alpha 0.4"
 TAGLINE = "Stay a while, and roll..."
 
+# Sets the minimum and maximum range for item weighting rolls
+MIN_WEIGHT = 1
+MAX_WEIGHT = 10000
+
+# Sets the minimum and maximum range for random power added to items
+MIN_POWER = 1
+MAX_POWER = 5
+
 def main():
-
- # Dictionary which lists item types and assigns them a number
+    # Dictionary which lists item types and assigns them a number
     item_types = {
-    "Weapon": 1,
-    "Armour": 2,
-    "Jewellery": 3
+        "Weapon": 1, "Armour": 2, "Jewellery": 3
     }
-
- # Dictionary which contains item prefixes and their associated dice modifiers
+    # Dictionary which records equipped items
+    equipped_items = {
+        "weapon_name": "0", "weapon_power": 0,
+        "armour_name": "0", "armour_power": 0,
+        "jewellery_name": "0", "jewellery_power": 0
+    }
+    # Dictionary which contains item prefixes and their associated power and drop weight (out of 10,000)
     item_prefixes = {
-    "Cursed": -10,
-    "Unlucky": -5,
-    "Trusty": 1,
-    "Fortunate": 2,
-    "Lucky" : 3,
-    "Uncanny" : 4,
-    "Weighted": 5,
-    "Blessed": 10
+        "Trusty": {"tier": 1, "power": 1, "weight": 5000},
+        "Fortunate": {"tier": 2, "power": 2, "weight": 3000},
+        "Lucky": {"tier": 3, "power": 3, "weight": 1250},
+        "Uncanny": {"tier": 4, "power": 4, "weight": 640},
+        "Weighted": {"tier": 5, "power": 5, "weight": 100},
+        "Blessed": {"tier": 6, "power": 10, "weight": 10},
+        "Destined": {"tier": 7, "power": 20, "weight": 1}
     }
-
- # Dictionary which contains material types and their associated dice modifiers
+    # Dictionary which contains material types and their associated power and drop weight (out of 10,000)
     item_materials = {
-    "Stone": -10,
-    "Wooden": -5,
-    "Copper": 1,
-    "Bronze": 2,
-    "Iron" : 3,
-    "Steel" : 4,
-    "Obsidian": 5,
-    "Mythril": 10
+        "Copper": {"tier": 1, "power": 1, "weight": 5000},
+        "Bronze": {"tier": 2, "power": 2, "weight": 3000},
+        "Iron": {"tier": 3, "power": 3, "weight": 1250},
+        "Steel": {"tier": 4, "power": 4, "weight": 640},
+        "Obsidian": {"tier": 5, "power": 5, "weight": 100},
+        "Mythril": {"tier": 6, "power": 10,  "weight": 10},
+        "Titanium": {"tier": 7, "power": 20, "weight": 1}
     }
-
- # Dictionary which contains weapon types and their associated dice modifiers
+    # Dictionary which contains weapon types and their associated power and drop weight (out of 10,000)
     weapon_types = {
-    "Pebble": -10,
-    "Stick": -5,
-    "Sword": 1,
-    "Axe": 2,
-    "Mace" : 3,
-    "Longbow" : 4,
-    "Katana": 5,
-    "Staff": 10
+        "Sword": {"tier": 1, "power": 1, "weight": 5000},
+        "Axe": {"tier": 2,  "power": 2, "weight": 3000},
+        "Mace": {"tier": 3, "power": 3, "weight": 1250},
+        "Longbow": {"tier": 4, "power": 4, "weight": 640},
+        "Katana": {"tier": 5, "power": 5, "weight": 100},
+        "Staff": {"tier": 6, "power": 10, "weight": 9},
+        "Tachi": {"tier": 7, "power": 20, "weight": 1}
     }
-
- # Dictionary which contains armour types and their associated dice modifiers
+    # Dictionary which contains armour types and their associated power and drop weight (out of 10,000)
     armour_types = {
-    "Pants": -10,
-    "Shirt": -5,
-    "Garb": 1,
-    "Tunic": 2,
-    "Cuirass" : 3,
-    "Hauberk" : 4,
-    "Mail": 5,
-    "Plate": 10
+        "Garb": {"tier": 1, "power": 1, "weight": 5000},
+        "Tunic": {"tier": 2, "power": 2, "weight": 3000},
+        "Cuirass": {"tier": 3, "power": 3, "weight": 1250},
+        "Hauberk": {"tier": 4, "power": 4, "weight": 640},
+        "Mail": {"tier": 5, "power": 5, "weight": 100},
+        "Plate": {"tier": 6, "power": 10, "weight": 9},
+        "Gosoku": {"tier": 7, "power": 20, "weight": 1},
     }
-
- # Dictionary which contains jewellery types and their associated dice modifiers
+    # Dictionary which contains jewellery types and their associated power and drop weight (out of 10,000)
     jewellery_types = {
-    "Trinket": -10,
-    "Charm": -5,
-    "Bangle": 1,
-    "Bracelet": 2,
-    "Earring" : 3,
-    "Ring" : 4,
-    "Amulet": 5,
-    "Crown": 10
+        "Bangle": {"tier": 1, "power": 1, "weight": 5000},
+        "Bracelet": {"tier": 2, "power": 2, "weight": 3000},
+        "Earring": {"tier": 3, "power": 3, "weight": 1250},
+        "Ring": {"tier": 4, "power": 4, "weight": 640},
+        "Amulet": {"tier": 5, "power": 5, "weight": 100},
+        "Crown": {"tier": 6, "power": 10, "weight": 10},
+        "Brooch": {"tier": 7, "power": 20, "weight": 1}
     }
-
- # Dictionary which contains item suffixes and their associated dice modifiers
+    # Dictionary which contains item suffixes and their associated power and drop weight (out of 10,000)
     item_suffixes = {
-    "Sloth": -10,
-    "Lemming": -5,
-    "Mouse": 1,
-    "Rabbit": 2,
-    "Dog" : 3,
-    "Cat" : 4,
-    "Parrot": 5,
-    "Dragon": 10
+        "Mouse": {"tier": 1, "power": 1, "weight": 5000},
+        "Rabbit": {"tier": 2, "power": 2, "weight": 3000},
+        "Dog": { "tier": 3, "power": 3, "weight": 1250},
+        "Cat": {"tier": 4, "power": 4, "weight": 640},
+        "Parrot": {"tier": 5, "power": 5, "weight": 100},
+        "Dragon": {"tier": 6, "power": 10, "weight": 10},
+        "Basilisk": {"tier": 7, "power": 20, "weight": 1}
     }
-
     # Prints a welcome message
     print(end='\n')
     print("Dieablo", VERSION, ":", TAGLINE)
@@ -122,19 +115,29 @@ def main():
     # Input prompt which commences and/or restarts the code
     while input("Roll the dice? Press enter.") == "":
 
-        # Generates an item tuple from the dictionary
-        def get_item(d):
-            random_key = random.choice(list(d.keys()))
-            return random_key, d[random_key]
+        def roll_item(dic):
+            # Roll a random number between defined weight range
+            roll = random.randint(MIN_WEIGHT, MAX_WEIGHT)
 
-        # Generate item attributes and corresponding power values
-        prefix, prefix_value = get_item(item_prefixes)
-        material, material_value = get_item(item_materials)
-        weapon, weapon_value = get_item(weapon_types)
-        armour, armour_value = get_item(armour_types)
-        jewellery, jewellery_value = get_item(jewellery_types)
-        suffix, suffix_value = get_item(item_suffixes)
+            # Create cumulative weights
+            cumulative = 0
+            cumulative_weights = []
 
+            for item, details in dic.items():
+                cumulative += details['weight']
+                cumulative_weights.append((cumulative, item, details['power']))
+
+            for cumulative_weight, item, power in cumulative_weights:
+                if roll <= cumulative_weight:
+                    return item, power
+
+        prefix, prefix_value = roll_item(item_prefixes)
+        material, material_value = roll_item(item_materials)
+        weapon, weapon_value = roll_item(weapon_types)
+        armour, armour_value = roll_item(armour_types)
+        jewellery, jewellery_value = roll_item(jewellery_types)
+        suffix, suffix_value = roll_item(item_suffixes)
+        
         # Generate complete names for items
         raw_weapon_name = (prefix, material, weapon, "of the", suffix)
         weapon_name = ' '.join(raw_weapon_name)
@@ -146,7 +149,7 @@ def main():
         jewellery_name = ' '.join(raw_jewellery_name)
 
         # Random power value to be added to item power
-        random_power = random.randint(1, 20)
+        random_power = random.randint(1, 10)
 
         # Compute combined powers for each item
         weapon_power = prefix_value + material_value + weapon_value + suffix_value + random_power
@@ -154,7 +157,7 @@ def main():
         jewellery_power = prefix_value + material_value + jewellery_value + suffix_value + random_power
 
         # Picks an item type
-        rolled_item_type = get_item(item_types)[0]
+        rolled_item_type = random.choice(list(item_types.keys()))
 
         # Defines the function which prints the resulting item
         def print_final_item():
@@ -165,7 +168,8 @@ def main():
                 print("Power level:", weapon_power)
                 print(end='\n')
                 print("(",prefix, prefix_value, "+", material, material_value, "+", weapon, weapon_value, "+",
-                suffix, suffix_value, "+", "Random Power (1-20)", random_power, "=", weapon_power,")")
+                suffix, suffix_value, "+", "Random Power (",MIN_POWER,"-",MAX_POWER,")",
+                      random_power, "=", weapon_power,")")
 
             elif rolled_item_type == "Armour":
                 print(end='\n')
@@ -174,7 +178,8 @@ def main():
                 print("Power level:", armour_power)
                 print(end='\n')
                 print("(",prefix, prefix_value, "+", material, material_value, "+", armour, armour_value, "+",
-                      suffix, suffix_value, "+", "Random Power (1-20)", random_power, "=", armour_power,")")
+                      suffix, suffix_value, "+", "Random Power (",MIN_POWER,"-",MAX_POWER,")",
+                      random_power, "=", armour_power,")")
 
             elif rolled_item_type == "Jewellery":
                 print(end='\n')
@@ -183,7 +188,8 @@ def main():
                 print("Power level:", jewellery_power)
                 print(end='\n')
                 print("(",prefix, prefix_value, "+", material, material_value, "+", jewellery, jewellery_value, "+",
-                      suffix, suffix_value, "+", "Random Power (1-20)", random_power, "=", jewellery_power,")")
+                      suffix, suffix_value, "+", "Random Power (",MIN_POWER,"-",MAX_POWER,")",
+                      random_power, "=", jewellery_power,")")
 
         # Call the function to print the resulting item
         print_final_item()
